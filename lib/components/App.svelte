1f046196969ec7f6afda0187567bea6efaa3aa85
Alexander Demin <ademin@gmail.com>
2023-06-12 15:25:13 +0100

    Initial

---
+++ lib/components/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Game from "./Game.svelte";
+</script>
+
+<div>
+    <Game />
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+    div {
+        height: 100vh;
+        width: 100rvw;
+        background: orange;
+    }
+</style>
